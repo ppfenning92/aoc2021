2820e09be762da38300a126d7f2436d7b453d4bd
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2022-12-04 15:33:38 +0100

    day 6

--- src/06/main.py
+++ src/06/main.py
@@ -31,12 +31,68 @@ from utils import Point, DIRS, DIRS_4, DIRS_8  # NOQA
 total = 0
 result = []
 table = new_table(None, width=2, height=4)
-data=[]
-with fileinput.input(files=(f"input.txt",), encoding="utf-8") as f:
-    for i, line in enumerate(f):
-        line = line.strip()
-        if line:
-            # data.append(int(line))
-            data.append({"line": i, "value": int(line)})
+data=None
 
-    print(data)
+test="""3,4,3,1,2"""
+with fileinput.input(files=(f"input.txt",), encoding="utf-8") as f:
+    # data = parse_nums(test)
+    data = parse_nums(f.readline())
+    # for i, line in enumerate(f):
+    #     line = line.strip()
+    #     if line:
+
+
+def day(val: int):
+    val -= 1
+    return val if val >= 0 else 6
+
+result = data
+for d in range(1,81):
+    new = result.count(0)
+    result = [day(fish) for fish in result]
+    if new > 0:
+        result.extend([8]*new)
+
+# assert len(result) == 5934
+print("part 1:", len(result))
+
+result = data
+
+state = {
+    0:0,
+    1:0,
+    2:0,
+    3:0,
+    4:0,
+    5:0,
+    6:0,
+    7:0,
+    8:0
+}
+for fish in data:
+    state[fish] += 1
+
+print(state)
+
+for d in range(1,257):
+    state = {
+        0: state[1],
+        1: state[2],
+        2: state[3],
+        3: state[4],
+        4: state[5],
+        5: state[6],
+        6: state[7] + state[0],
+        7: state[8],
+        8: state[0]
+    }
+
+    print(state)
+
+print(sum(state.values()))
+
+# assert len(result) == 26984457539
+
+
+
+
